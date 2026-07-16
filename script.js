08ed873a4153e7c1b17d907cb1b75022faff10c7
enoch-philip <enochphilipurla@gmail.com>
2026-07-16 13:53:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,19 +38,30 @@ function closeMedia(){
 // LETTER TYPING
 // =========================
 
-const letterText = `Happy Birthday, My Dear ❤️
+const letterText = `Happy Birthday, little kid❤️
 
-Today is all about celebrating you.
+Today is all about celebrating you🫶🏻.
 
-Thank you for every smile, every laugh, and every beautiful memory we've shared.
 
-I hope this year brings you endless happiness, success, peace, and all the love your heart deserves.
+All the love your heart deserves.
 
 No matter what happens, I will always cherish every moment we've spent together.
+Im really glad that i met such idiot😁.. you've become one of my favorite people to talk to. 
+From playing badminton together to sharing random thoughts and conversations, 
+every moment has been special.
+ofcourse i might hurt u sometimes (im sorry 🤧),
+edipinchify u sometimes(asa dosa i wont stop kelking u so don't expect👹hee)
+we fight,we cry but in the end we CARE..i love the way u be with yourself and me ..
+i really hope we do make lot more memories together and have lots of fun 😄
+also i promise u [I'll always be the BEST for you] ..i wish u overcome all your fears,scars 
+and my support always gonna be your side 💪🏻
+Thank you for being such a wonderful friend. I hope this year brings you happiness, 
+success, good health, and countless reasons to smile. Keep being the amazing person you are.
+ Have a fantastic day—you truly deserve it. 🎂
 
-Keep smiling, keep shining, and keep being the wonderful person you are.
+Keep smiling, keep shining
 
-Happy Birthday once again!
+A very Happy Birthday to the little one once again!🎀💗
 
 ❤️ Forever wishing you the very best ❤️`;
 
